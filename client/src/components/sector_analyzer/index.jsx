10b4dbf612f4e6a1794dc867d29708c9da88f4e5
M Irfan <naprirfan@gmail.com>
2020-05-31 09:57:45 +0200

    Add materialUI table

--- client/src/components/sector_analyzer/index.jsx
+++ client/src/components/sector_analyzer/index.jsx
@@ -8,22 +8,11 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 class SectorAnalyzer extends Component {
     state = {
-        data: {},
+        data: [],
     };
 
     componentDidMount() {
@@ -33,6 +22,8 @@ class SectorAnalyzer extends Component {
     }
 
     render() {
+        if (!this.state.data.length) return <CircularProgress />;
+
         return (
             <>
                 <Typography variant="h2" gutterBottom>
@@ -46,32 +37,28 @@ class SectorAnalyzer extends Component {
                     <Table aria-label="simple table">
                         <TableHead>
                             <TableRow>
-                                <TableCell>Dessert (100g serving)</TableCell>
-                                <TableCell align="right">Calories</TableCell>
-                                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                                <TableCell>Rank</TableCell>
+                                <TableCell>Stock code</TableCell>
+                                <TableCell align="right">ROA</TableCell>
+                                <TableCell align="right">Other indicator</TableCell>
+                                <TableCell align="right">Fix this</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {rows.map((row) => (
+                            {this.state.data.map((row, index) => (
                                 <TableRow key={row.name}>
                                     <TableCell component="th" scope="row">
-                                        {row.name}
+                                        {index}
                                     </TableCell>
-                                    <TableCell align="right">{row.calories}</TableCell>
-                                    <TableCell align="right">{row.fat}</TableCell>
-                                    <TableCell align="right">{row.carbs}</TableCell>
-                                    <TableCell align="right">{row.protein}</TableCell>
+                                    <TableCell>{row.name}</TableCell>
+                                    <TableCell align="right">{row.mass}</TableCell>
+                                    <TableCell align="right">{row.birth_year}</TableCell>
+                                    <TableCell align="right">{row.eye_color}</TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
                     </Table>
                 </TableContainer>
-
-                <pre>
-                    { JSON.stringify(this.state.data, null, 2) }
-                </pre>
             </>
         )
     }
